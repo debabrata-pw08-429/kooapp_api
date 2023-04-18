11c2bc4b75c75d57f4cba2443b6de7927828d2b8
debabrata-pw08-429 <chintuthecoder@gmail.com>
2023-04-18 13:46:44 +0530

    Dependencies Added

--- Configs/connection.js
+++ Configs/connection.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 const CONNECT = async () => {
-  await mongoose.connect("mongodb://localhost:27017/Koodata");
+  const URL = process.env.DB_URL;
+  await mongoose.connect(URL);
 };
 
 module.exports = CONNECT;
